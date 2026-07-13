68ec7509f7dd2ba95429d01ad31175d9cd3e2634
m <m@mail.com>
2026-07-13 13:20:14 +0200

    remove folder notion

--- backend/internal/db/migrations/003_tags.up.sql
+++ backend/internal/db/migrations/003_tags.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE tags (
   id          TEXT PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   parent_tag_id TEXT,  
   tag_name    TEXT NOT NULL, -- exemple: Brice, vélo, facture ...
-  tag_type    TEXT NOT NULL DEFAULT 'none', -- exemple: entity, none, folder
+  tag_type    TEXT NOT NULL DEFAULT 'none', -- exemple: entity, none
   created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT fk_parent_tag FOREIGN KEY(parent_tag_id) REFERENCES tags(id)
